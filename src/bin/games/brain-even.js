4b91ef4ccd96003c480252c5da367f7cb0ce0ae5
Andrey Shimkov <zark2002@mail.ru>
2018-09-11 21:16:43 +0300

    v 1.4.0

--- src/bin/games/brain-even.js
+++ src/bin/games/brain-even.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-import { askName, brainGamesEven } from '..';
+import { askName, brainGamesEven } from '../..';
 
 console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no"');
 const name = askName();
-const rounds = 3;
 
-brainGamesEven(name, rounds);
+brainGamesEven(name);
 
 console.log(`Congratulations, ${name}`);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 
 import readlineSync from 'readline-sync';
 
+const rounds = 3;
+const maxNumber = 100;
+const maxOperations = 3;
+
 export const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
   return userName;
 };
 
-const checkAnswer = (answer, correctAnswer, name) => {
+const checkAnswer = (question, correctAnswer, name) => {
+  console.log(`Question: ${question}`);
+  const answer = readlineSync.question('Your answer: ');
   if (answer === correctAnswer) {
     console.log('Correct!');
   } else {
@@ -16,19 +22,42 @@ const checkAnswer = (answer, correctAnswer, name) => {
   }
 };
 
-const askQuestion = (num, name) => {
-  console.log(`Question: ${num}`);
-  const answer = readlineSync.question('Your answer: ');
-  const correctAnswer = num % 2 !== 0 || num === 0 ? 'no' : 'yes';
-  checkAnswer(answer, correctAnswer, name);
+const generateRandomNumber = num => Math.floor(Math.random() * num + 1);
+
+export const brainGamesCalc = (name) => {
+  let number1;
+  let number2;
+  let operation;
+  let correctAnswer;
+  let question;
+  for (let i = 1; i <= rounds; i += 1) {
+    number1 = generateRandomNumber(maxNumber);
+    number2 = generateRandomNumber(maxNumber);
+    operation = generateRandomNumber(maxOperations);
+    switch (operation) {
+      case 1:
+        question = `${number1} + ${number2}`;
+        correctAnswer = number1 + number2;
+        break;
+      case 2:
+        question = `${number1} - ${number2}`;
+        correctAnswer = number1 - number2;
+        break;
+      default:
+        question = `${number1} * ${number2}`;
+        correctAnswer = number1 * number2;
+        break;
+    }
+    checkAnswer(question, correctAnswer, name);
+  }
 };
 
-const generateRandomNumber = () => Math.floor(Math.random() * 30);
-
-export const brainGamesEven = (name, rounds) => {
-  let number;
+export const brainGamesEven = (name) => {
+  let question;
+  let correctAnswer;
   for (let i = 1; i <= rounds; i += 1) {
-    number = generateRandomNumber();
-    askQuestion(number, name);
+    question = generateRandomNumber(maxNumber);
+    correctAnswer = question % 2 !== 0 ? 'no' : 'yes';
+    checkAnswer(question, correctAnswer, name);
   }
 };
