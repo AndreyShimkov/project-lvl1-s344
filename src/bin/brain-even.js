92a87b1162fc7a949b03c1681eda9e19b51b6e34
Andrey Shimkov <zark2002@mail.ru>
2018-09-11 14:14:05 +0300

    fourth change v1.2

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,33 +1,12 @@
 #!/usr/bin/env node
 
-import askName from '..';
-import readlineSync from 'readline-sync';
+import { askName, brainGamesEven } from '..';
 
 console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no"');
 const name = askName();
+const rounds = 3;
 
-const checkAnswer = (answer, correctAnswer) => {
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
-    console.log(`Let's try again, ${name}`);
-  }
-};
-const askQuestion = (num) => {
-  console.log(`Question: ${num}`);
-  const answer = readlineSync.question('Your answer: ');
-  const correctAnswer = num % 2 !== 0 || num === 0 ? 'no' : 'yes';
-  checkAnswer(answer, correctAnswer);
-};
-const main = () => {
-  let number;
-  for (let i = 1; i < 3; i += 1) {
-    number = Math.random() * 100;
-    askQuestion(number);
-  }
-};
+brainGamesEven(name, rounds);
 
-main();
 console.log(`Congratulations, ${name}`);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
   return userName;
+};
+
+const checkAnswer = (answer, correctAnswer, name) => {
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+    console.log(`Let's try again, ${name}`);
+  }
+};
+
+const askQuestion = (num, name) => {
+  console.log(`Question: ${num}`);
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = num % 2 !== 0 || num === 0 ? 'no' : 'yes';
+  checkAnswer(answer, correctAnswer, name);
+};
+
+const generateRandomNumber = () => Math.random() * 100;
+
+export const brainGamesEven = (name, rounds) => {
+  let number;
+  for (let i = 1; i < rounds; i += 1) {
+    number = generateRandomNumber();
+    askQuestion(number, name);
+  }
 };
