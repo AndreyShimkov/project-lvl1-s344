4c56a52b9aa19313253d7130180455453ca4160c
Andrey Shimkov <zark2002@mail.ru>
2018-09-12 19:30:45 +0300

    v 1.7.4

--- src/bin/games/brain-even-game.js
+++ src/bin/games/brain-even-game.js
@@ -6,6 +6,6 @@ export const gameRules = 'Answer "yes" if number even otherwise answer "no"';
 export const brainGamesEven = (name) => {
   const question = generateRandomNumber();
   const userAnswer = askQuestion(question);
-  const correctAnswer = number => (number % 2 !== 0 ? 'no' : 'yes');
+  const correctAnswer = question % 2 !== 0 ? 'no' : 'yes';
   checkAnswer(userAnswer, correctAnswer, name);
 };
