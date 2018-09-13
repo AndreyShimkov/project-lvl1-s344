09f6122525a582e96d919d449bca3863ab1ae5f6
Andrey Shimkov <zark2002@mail.ru>
2018-09-13 08:56:20 +0300

    test commit v1

--- src/bin/games/brain-even-game.js
+++ src/bin/games/brain-even-game.js
@@ -3,9 +3,10 @@ import { brainGamesMain, generateRandomNumber, cons } from '../..';
 
 const gameRules = 'Answer "yes" if number even otherwise answer "no"';
 
-const question = generateRandomNumber();
-const correctAnswer = question % 2 !== 0 ? 'no' : 'yes';
-
-const brainGamesEven = () => cons(question, correctAnswer);
+const brainGamesEven = () => {
+  const question = generateRandomNumber();
+  const correctAnswer = question % 2 !== 0 ? 'no' : 'yes';
+  return cons(question, correctAnswer);
+};
 
 export default () => brainGamesMain(gameRules, brainGamesEven);
