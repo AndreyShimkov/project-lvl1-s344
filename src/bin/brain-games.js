6ab6dc30440114f8c47437aea6c4d28ba2786e21
Andrey Shimkov <zark2002@mail.ru>
2018-09-14 23:11:11 +0300

    update menu

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import { askName } from '..';
+import menu from '../modules/brain-menu-modules';
 
-console.log('Welcome to the Brain Games!');
-
-askName();
+menu();
--- src/bin/brain-menu.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import menu from '../modules/brain-menu-modules';
-
-menu();
